6051ddac68cc41f9623eecb101882c0b447f6f9c
Moontary <kostiantyntkachenko@gmail.com>
2022-08-05 21:43:17 +0300

    refactor: remove old functionality

--- backend/internal/model/model.go
+++
@@ -1,8 +0,0 @@
-package model
-
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
-type Email struct {
-	ID    primitive.ObjectID `json:"id" bson:"_id,omitempty"`
-	Email string             `json:"email" bson:"email,omitempty" validate:"required,email"`
-}
